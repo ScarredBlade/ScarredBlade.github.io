dcdf1f09a48232191826abf52c29049e02a51a9e
ScarredBlade <111335402+ScarredBlade@users.noreply.github.com>
2023-08-07 14:27:08 +0800

    Web Dev ICA2

--- webdevA2/script.js
+++ webdevA2/script.js
@@ -1,10 +1,10 @@
 function showPage(topic) {
   // Hide all topic divs
   const topics = document.querySelectorAll('#main-content > div');
-  topics.forEach((topicDiv) => {
+  for(let topicDiv of topics) {
     topicDiv.style.display = 'none';
     topicDiv.style.transform = 'translateX(-100%)'; // Add this line for the transition effect
-  });
+  }
 
   // Show the selected topic div with transition
   const selectedTopic = document.getElementById(topic);
@@ -13,86 +13,98 @@ function showPage(topic) {
 
   // Remove the "vibrate" class from all images
   const images = document.querySelectorAll('img');
-  images.forEach((image) => {
+  for(let image of images) {
     image.classList.remove('vibrate');
-  });
+  }
 
   // Add the "vibrate" class to images under topics that are not "game"
   if (topic !== 'game') {
     const topicImages = selectedTopic.querySelectorAll('img');
-    topicImages.forEach((image) => {
+    for(let image of topicImages) {
       image.classList.add('vibrate');
-    });
+    }
   }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+  // Topics
+  document.querySelectorAll("#menu a")[0].addEventListener("click", function(){showPage("breeds");});
+  document.querySelectorAll("#menu a")[1].addEventListener("click", function(){showPage("adoption");});
+  document.querySelectorAll("#menu a")[2].addEventListener("click", function(){showPage("fun");});
+  document.querySelectorAll("#menu a")[3].addEventListener("click", function(){showPage("game");});
+
   // Breeds
   const breedCards = document.querySelectorAll('.breed-card');
-  breedCards.forEach((breedCard) => {
+  for(let breedCard of breedCards) {
     breedCard.addEventListener('click', showContent);
-  });
-
-  function showContent(event) {
-    const breedCard = event.currentTarget;
-    const h3 = breedCard.querySelector('h3');
-    const p = breedCard.querySelector('p');
-
-    fadeIn(h3);
-    fadeIn(p);
-  }
-
-  function fadeIn(element) {
-    element.style.opacity = 0;
-    element.style.display = 'block';
-
-    let opacity = 0;
-    const fadeInInterval = setInterval(function () {
-      opacity += 0.1;
-      element.style.opacity = opacity;
-
-      if (opacity >= 1) {
-        clearInterval(fadeInInterval);
-      }
-    }, 100);
   }
 
   // Adopt
   const adoptCards = document.querySelectorAll('.clickable-card');
-  adoptCards.forEach((adoptCard) => {
-    const icon = adoptCard.querySelector('.icon');
+  for (let adoptCard of adoptCards) {
+    const icon = adoptCard.querySelector('.icon');  
+    icon.addEventListener('click', createClickListener(adoptCard));
+  }  
 
-    icon.addEventListener('click', function () {
-      const h3 = adoptCard.querySelector('h3');
-      const p = adoptCard.querySelector('p');
-      const ul = adoptCard.querySelector('ul');
-
-      slideIn(h3);
-      slideIn(p);
-      slideIn(ul);
-    });
-
-    function slideIn(element) {
-      element.style.display = 'block';
-      element.style.transform = 'translateX(-100%)'; // Start from the left (slide in position)
-
-      let distance = 100; // Start position for slide-in animation (100%)
-      const slideInInterval = setInterval(function () {
-        distance -= 2; // Decrease the distance to slide in (from 100% to 0%)
-        element.style.transform = `translateX(-${distance}%)`;
-
-        if (distance <= 0) {
-          clearInterval(slideInInterval);
-        }
-      }, 20); // Lowered interval (20) for smoother animation
-    }
-  });
+  document.querySelector("#playButton").addEventListener("click", function(){startGame();});
 });
+
+// Breeds
+function showContent(event) {
+  const breedCard = event.currentTarget;
+  const h3 = breedCard.querySelector('h3');
+  const p = breedCard.querySelector('p');
+
+  fadeIn(h3);
+  fadeIn(p);
+}
+
+function fadeIn(element) {
+  element.style.opacity = 0;
+  element.style.display = 'block';
+
+  let opacity = 0;
+  const fadeInInterval = setInterval(function () {
+    opacity += 0.1;
+    element.style.opacity = opacity;
+
+    if (opacity >= 1) {
+      clearInterval(fadeInInterval);
+    }
+  }, 100);
+}
+
+// Adopt
+function slideIn(element) {
+  element.style.display = 'block';
+  element.style.transform = 'translateX(-100%)'; // Start from the left (slide in position)
+
+  var distance = 100; // Start position for slide-in animation (100%)
+  var slideInInterval = setInterval(function () {
+    distance -= 2; // Decrease the distance to slide in (from 100% to 0%)
+    element.style.transform = 'translateX(-' + distance + '%)';
+
+    if (distance <= 0) {
+      clearInterval(slideInInterval);
+    }
+  }, 20); // Lowered interval (20) for smoother animation
+}
+
+function createClickListener(adoptCard) {
+  return function () {
+    const h3 = adoptCard.querySelector('h3');
+    const p = adoptCard.querySelector('p');
+    const ul = adoptCard.querySelector('ul');
+
+    slideIn(h3);
+    slideIn(p);
+    slideIn(ul);
+  };
+}
 
 let timer;
 let score = 0;
 let isGameStarted = false;
-const catContainer = document.querySelector('.cat-container');
 
 function startGame() {
   const cat = document.getElementById('cat');
@@ -118,20 +130,20 @@ function startGame() {
 }
 
 function moveCat(cat) {
-  const containerSize = document.querySelector('.cat-container').offsetWidth;
-  const catSize = cat.offsetWidth;
-  const maxX = containerSize - catSize;
-  const maxY = containerSize - catSize;
+  var containerSize = document.querySelector('.cat-container').offsetWidth;
+  var catSize = cat.offsetWidth;
+  var maxX = containerSize - catSize;
+  var maxY = containerSize - catSize;
 
   // Move the cat randomly within the container
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
+  var randomX = Math.floor(Math.random() * maxX);
+  var randomY = Math.floor(Math.random() * maxY);
 
   cat.style.left = randomX + 'px';
   cat.style.top = randomY + 'px';
 
   // Wait for a short duration and move again
-  setTimeout(() => {
+  setTimeout(function () {
     if (isGameStarted) {
       moveCat(cat);
     }
@@ -139,9 +151,9 @@ function moveCat(cat) {
 }
 
 function startTimer() {
-  const timerElement = document.getElementById('timer');
-  let timeLeft = 60;
-  timer = setInterval(() => {
+  var timerElement = document.getElementById('timer');
+  var timeLeft = 60;
+  timer = setInterval(function () {
     timeLeft--;
     timerElement.textContent = 'Time: ' + timeLeft;
 
